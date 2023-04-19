3ddcac6c7c29c80082391923fef33df3698a288f
bubble3jh <bubble3jh@naver.com>
2023-04-19 08:19:18 +0000

    args.emb add

--- sh/linear.sh
+++ sh/linear.sh
@@ -9,7 +9,7 @@ do
     do
         for scaling in 'minmax' 'normalization'
         do
-        CUDA_VISIBLE_DEVICES=6 python3 main.py --model=linear --optim=adam --lr_init=${lr_init} --wd=${wd} --epochs=10 --scheduler=constant --scaling=${scaling}
+        CUDA_VISIBLE_DEVICES=6 python3 main.py --model=linear --optim=adam --lr_init=${lr_init} --wd=${wd} --epochs=100 --scheduler=constant --scaling=${scaling}
         done
     done
 done
@@ -21,7 +21,7 @@ do
     do
         for scaling in 'minmax' 'normalization'
         do
-        CUDA_VISIBLE_DEVICES=6 python3 main.py --model=linear --optim=adam --lr_init=${lr_init} --wd=${wd} --epochs=10 --scheduler=cos_anneal --t_max=10 --scaling=${scaling}
+        CUDA_VISIBLE_DEVICES=6 python3 main.py --model=linear --optim=adam --lr_init=${lr_init} --wd=${wd} --epochs=100 --scheduler=cos_anneal --t_max=10 --scaling=${scaling}
         done
     done
 done
