67928b8dd2470a5b48e121caa3ebcabe1ccf5f5b
bubble3jh <bubble3jh@naver.com>
2023-04-19 06:29:44 +0000

    dd

--- models.py
+++ models.py
@@ -1,10 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from typing import Optional
-
-
+import math
+import pdb
     
 
 class MLPRegressor(nn.Module):
@@ -34,6 +33,56 @@ class LinearRegression(torch.nn.Module):
         x = self.linear1(x)
         return x
 
+
+class TSTransformer(torch.nn.Module):
+    def __init__(self, hidden_size=16, output_size=16):
+        super().__init__()
+        self.linear_cont_P = nn.Sequential(
+            nn.Linear(3, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, 3 * output_size)
+        )
+        self.embedding_p = nn.Embedding(5, output_size)
+        self.linear_cont_C = nn.Sequential(
+            nn.Linear(2, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, 2 * output_size)
+        )
+        self.embedding_c = nn.Embedding(2, output_size)
+        # self.transformer = PETransformer(nhead=16, num_encoder_layers=12)
+    
+    def forward(self, cont_P, disc_P, cont_C, disc_C):
+        cont_P_out = self.linear_cont_P(cont_P)
+        cont_P_out = cont_P_out.view(-1, 3, 16)
+        disc_P_out = self.embedding_p(disc_P)
+        
+        p_out = torch.cat((cont_P_out.unsqueeze(1), disc_P_out.unsqueeze(1)), dim=1)
+        p_out = torch.mean(p_out, dim=1)
+        
+        cont_C_out = self.linear_cont_C(cont_C)
+        cont_C_out = cont_C_out.view(-1, 2, 16)
+        disc_C_out = self.embedding_c(disc_C)
+        c_out = torch.cat((cont_C_out.unsqueeze(1), disc_C_out.unsqueeze(1)), dim=1)
+        c_out = torch.mean(c_out, dim=1)
+        
+        pdb.set_trace()
+        out = torch.add(p_out, c_out)
+
+        return out
+
+class PETransformer(nn.Transformer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Modified Positional Encoding
+        self.pos_encoder = nn.Embedding(d_model, d_model)
+
+    def forward(self, src, tgt):
+        src = self.encoder(src) * math.sqrt(self.d_model)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src, src_mask)
+        output = self.decoder(output)
+        return output
 
 class SVR(torch.nn.Module):
     def __init__(self):
--- utils.py
+++ utils.py
@@ -56,38 +56,63 @@ class Tabledata(Dataset):
         y = torch.tensor(self.y[index])
         return binary_X, cont_X, cat_X, y
 
-
-'''
 class Seqdata(Dataset):
     def __init__(self, data):
-        self.binary_X = data.iloc[:, 1:4].values.astype('float32')
-        self.cont_X = data.iloc[:, 4:10].values.astype('float32')
-        self.cat_X = data.iloc[:, 10:14].astype('category')
-        self.y = data.iloc[:, -1].values.astype('float32')
+        for c in ["age", "dis", "danger", "CT_R", "CT_E"]:
+            # minmax_col(data, c)
+            meanvar_col(data, c)
+        # miny, maxy = minmax_col(data,"y")
+        # meany, vary = meanvar_col(data, "y")
+
+        sub_y = []
+        for _, group in data.groupby('cluster'):
+            group['sub_y'] = group['diff_days'].map(group['diff_days'].value_counts())
+            sub_y.extend(group['sub_y'].tolist())
+        data['sub_y'] = sub_y
+    
+        data=data[['cluster', 'age',  'CT_R', 'CT_E', 'gender', 'is_korean', 'primary case', 'job_idx','rep_idx', # patient datas
+                   'dis', 'danger', 'place_idx', 'add_idx', 'y', 'diff_days', 'sub_y']]                           # cluster datas
+        data = data.sort_values(by=["cluster", "diff_days"], ascending=[True, True])
+        
+        self.cont_P = data.iloc[:, 1:4].values.astype('float32') 
+        self.disc_P = data.iloc[:, 4:9].astype('category')
+        self.cont_C = data.iloc[:, 9:11].values.astype('float32') 
+        self.disc_C = data.iloc[:, 11:13].astype('category')
+        
+        self.y = data.iloc[:, -2:].values.astype('float32')
 
         # 범주형 데이터 처리 - 0~n 까지로 맞춰줌
-        self.cat_cols = self.cat_X.columns
-        self.cat_map = {col: {cat: i for i, cat in enumerate(self.cat_X[col].cat.categories)} for col in self.cat_cols}
-        self.cat_X = self.cat_X.apply(lambda x: x.cat.codes)
-        self.cat_X = torch.from_numpy(self.cat_X.to_numpy()).long()
-        self.embeddings = nn.ModuleList([nn.Embedding(len(self.cat_map[col]), 4) for col in self.cat_cols])
+        self.disc_P_cols = self.disc_P.columns
+        self.disc_P_map = {col: {cat: i for i, cat in enumerate(self.disc_P[col].cat.categories)} for col in self.disc_P_cols}
+        self.disc_P = self.disc_P.apply(lambda x: x.cat.codes)
+        self.disc_P = torch.from_numpy(self.disc_P.to_numpy()).long()
+        
+        self.disc_C_cols = self.disc_C.columns
+        self.disc_C_map = {col: {cat: i for i, cat in enumerate(self.disc_C[col].cat.categories)} for col in self.disc_C_cols}
+        self.disc_C = self.disc_C.apply(lambda x: x.cat.codes)
+        self.disc_C = torch.from_numpy(self.disc_C.to_numpy()).long()
 
     def __len__(self):
-        return len(self.cont_X)
+        return len(self.cont_P)
 
     def __getitem__(self, index):
-        cont_X = torch.from_numpy(self.cont_X[index])
-        cat_X = self.cat_X[index]
-        # batch size 가 1일 때 예외 처리
-        if len(cat_X.shape) == 1:
-            embeddings = [embedding(cat_X[i].unsqueeze(0)) for i, embedding in enumerate(self.embeddings)]
-        else:
-            embeddings = [embedding(cat_X[:, i]) for i, embedding in enumerate(self.embeddings)]
-        cat_X = torch.cat(embeddings, 1).squeeze()
-        binary_X = torch.from_numpy(self.binary_X[index])
+        cont_C = torch.from_numpy(self.cont_C[index])
+        cont_P = torch.from_numpy(self.cont_P[index])
+        disc_C = self.disc_C[index]
+        disc_P = self.disc_P[index]
+        # # batch size 가 1일 때 예외 처리
+        # if len(disc_C.shape) == 1:
+        #     embeddings = [embedding(disc_C[i].unsqueeze(0)) for i, embedding in enumerate(self.embeddings)]
+        # else:
+        #     embeddings = [embedding(disc_C[:, i]) for i, embedding in enumerate(self.embeddings)]
+        # disc_C = torch.cat(embeddings, 1).squeeze()
+        # if len(disc_P.shape) == 1:
+        #     embeddings = [embedding(disc_P[i].unsqueeze(0)) for i, embedding in enumerate(self.embeddings)]
+        # else:
+        #     embeddings = [embedding(disc_P[:, i]) for i, embedding in enumerate(self.embeddings)]
+        # disc_P = torch.cat(embeddings, 1).squeeze()
         y = torch.tensor(self.y[index])
-        return binary_X, cont_X, cat_X, y
-'''
+        return cont_P, disc_P, cont_C, disc_C, y
 
 ## MinMax Scaling Functions ------------------------------------
 def minmax_col(data,name):
@@ -131,28 +156,49 @@ class RMSELoss(nn.Module):
 ## Train --------------------------------------------------------------------------------------
 def train(data, model, optimizer, criterion, lamb=0.0):
     model.train()
-    binary_X, cont_X, cat_X, y = data
-    data_x = torch.cat((binary_X, cont_X, cat_X), dim=1).cuda()
-    y=y.cuda()
-    optimizer.zero_grad()
-    batch_num = data_x.shape[0]
-    out = model(data_x)
-    
-    loss1 = criterion(out[:,0], y[:,0])
-    loss2 = criterion(out[:,1], y[:,1])
-    loss = loss1 + loss2
-    
-    # Add Penalty term for ridge regression
-    if lamb != 0.0:
-        loss += lamb * torch.norm(model.linear1.weight, p=2)
-    
-    if not torch.isnan(loss):
-      loss.backward()
-      optimizer.step()
-      return loss.item(), batch_num, out, y
-    else:
-      return 0, batch_num, out, y
-
+    if model.__class__.__name__ != "TSTransformer" :
+        binary_X, cont_X, cat_X, y = data
+        data_x = torch.cat((binary_X, cont_X, cat_X), dim=1).cuda()
+        y=y.cuda()
+        optimizer.zero_grad()
+        batch_num = data_x.shape[0]
+        out = model(data_x)
+        
+        loss1 = criterion(out[:,0], y[:,0])
+        loss2 = criterion(out[:,1], y[:,1])
+        loss = loss1 + loss2
+        
+        # Add Penalty term for ridge regression
+        if lamb != 0.0:
+            loss += lamb * torch.norm(model.linear1.weight, p=2)
+        
+        if not torch.isnan(loss):
+            loss.backward()
+            optimizer.step()
+            return loss.item(), batch_num, out, y
+        else:
+            return 0, batch_num, out, y
+    else :
+        cont_P, disc_P, cont_C, disc_C, y = data
+        cont_P, disc_P, cont_C, disc_C, y = cont_P.to("cuda"), disc_P.to("cuda"), cont_C.to("cuda"), disc_C.to("cuda"), y.to("cuda")
+        optimizer.zero_grad()
+        batch_num = cont_P.shape[0]
+        out = model(cont_P, disc_P, cont_C, disc_C)
+        
+        # loss1 = criterion(out[:,0], y[:,0])
+        # loss2 = criterion(out[:,1], y[:,1])
+        # loss = loss1 + loss2
+        
+        # # Add Penalty term for ridge regression
+        # if lamb != 0.0:
+        #     loss += lamb * torch.norm(model.linear1.weight, p=2)
+        
+        if not torch.isnan(loss):
+        #     loss.backward()
+        #     optimizer.step()
+            return loss.item(), batch_num, out, y
+        else:
+            return 0, batch_num, out, y
 
 ## Validation --------------------------------------------------------------------------------
 @torch.no_grad()
