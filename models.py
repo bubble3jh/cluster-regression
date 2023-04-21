6d2bcf394c029171263770c279df8ced2ee8769c
bubble3jh <bubble3jh@naver.com>
2023-04-21 11:36:39 +0000

    emb mod

--- models.py
+++ models.py
@@ -53,29 +53,28 @@ class TableEmbedding(torch.nn.Module):
             self.cont_c_NN = nn.Sequential(nn.Linear(2, emb_hidden_dim),
                                         nn.ReLU(),
                                         nn.Linear(emb_hidden_dim, emb_hidden_dim))
-            self.lookup_gender  = nn.Embedding(2, emb_hidden_dim).to('cuda:0')
-            self.lookup_korean  = nn.Embedding(2, emb_hidden_dim).to('cuda:0')
-            self.lookup_primary  = nn.Embedding(2, emb_hidden_dim).to('cuda:0')
-            self.lookup_job  = nn.Embedding(11, emb_hidden_dim).to('cuda:0')
-            self.lookup_rep  = nn.Embedding(34, emb_hidden_dim).to('cuda:0')
-            self.lookup_place  = nn.Embedding(19, emb_hidden_dim).to('cuda:0')
-            self.lookup_add  = nn.Embedding(31, emb_hidden_dim).to('cuda:0')
+        self.lookup_gender  = nn.Embedding(2, emb_hidden_dim).to('cuda:0')
+        self.lookup_korean  = nn.Embedding(2, emb_hidden_dim).to('cuda:0')
+        self.lookup_primary  = nn.Embedding(2, emb_hidden_dim).to('cuda:0')
+        self.lookup_job  = nn.Embedding(11, emb_hidden_dim).to('cuda:0')
+        self.lookup_rep  = nn.Embedding(34, emb_hidden_dim).to('cuda:0')
+        self.lookup_place  = nn.Embedding(19, emb_hidden_dim).to('cuda:0')
+        self.lookup_add  = nn.Embedding(31, emb_hidden_dim).to('cuda:0')
 
     def forward(self, cont_p, cont_c, cat_p, cat_c, len):
         if self.apply_embedding:
-            a1_embs = self.lookup_gender(cat_p[:,:,0].to(torch.int))
-            a2_embs = self.lookup_korean(cat_p[:,:,1].to(torch.int))
-            a3_embs = self.lookup_primary(cat_p[:,:,2].to(torch.int))
-            a4_embs = self.lookup_job(cat_p[:,:,3].to(torch.int))
-            a7_embs = self.lookup_rep(cat_p[:,:,4].to(torch.int))
-            a5_embs = self.lookup_place(cat_c[:,:,0].to(torch.int))
-            a6_embs = self.lookup_add(cat_c[:,:,1].to(torch.int))
-            # categorical datas embedding 평균
-            cat_p = torch.mean(torch.stack([a1_embs, a2_embs, a3_embs, a4_embs, a5_embs]), axis=0)
-            cat_c = torch.mean(torch.stack([a6_embs, a7_embs]), axis=0)
-            
             cont_p = self.cont_p_NN(cont_p)
             cont_c = self.cont_c_NN(cont_c)
+        a1_embs = self.lookup_gender(cat_p[:,:,0].to(torch.int))
+        a2_embs = self.lookup_korean(cat_p[:,:,1].to(torch.int))
+        a3_embs = self.lookup_primary(cat_p[:,:,2].to(torch.int))
+        a4_embs = self.lookup_job(cat_p[:,:,3].to(torch.int))
+        a7_embs = self.lookup_rep(cat_p[:,:,4].to(torch.int))
+        a5_embs = self.lookup_place(cat_c[:,:,0].to(torch.int))
+        a6_embs = self.lookup_add(cat_c[:,:,1].to(torch.int))
+        # categorical datas embedding 평균
+        cat_p = torch.mean(torch.stack([a1_embs, a2_embs, a3_embs, a4_embs, a5_embs]), axis=0)
+        cat_c = torch.mean(torch.stack([a6_embs, a7_embs]), axis=0)
         x = torch.cat((cat_p, cat_c, cont_p, cont_c), dim=2)
         sliced_tensors = []
         for i in range(x.shape[0]):
