31bc821b619f1c001a04a500d1126503c398c95c
bubble3jh <bubble3jh@naver.com>
2023-04-24 03:33:15 +0000

    add transformer

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from typing import Optional
 import math
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
 import pdb
     
 
@@ -85,51 +86,66 @@ class TableEmbedding(torch.nn.Module):
         x = torch.stack(sliced_tensors, dim=0)
         return x
 
-class TSTransformer(torch.nn.Module):
-    def __init__(self, hidden_size=16, output_size=16):
+class TransformerModel(nn.Module):
+
+    def __init__(self, ntoken: int, d_model: int, nhead: int, d_hid: int,
+                 nlayers: int, dropout: float = 0.5):
         super().__init__()
-        self.linear_cont_P = nn.Sequential(
-            nn.Linear(3, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, output_size)
-        )
-        self.embedding_p = nn.Embedding(5, output_size)
-        self.linear_cont_C = nn.Sequential(
-            nn.Linear(2, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, output_size)
-        )
-        self.embedding_c = nn.Embedding(2, output_size)
-        # self.transformer = PETransformer(nhead=16, num_encoder_layers=12)
-        
-    def forward(self, cont_P, disc_P, cont_C, disc_C):
-        cont_P_out = self.linear_cont_P(cont_P)
-        disc_P_out = self.embedding_p(disc_P)
-        p_out = torch.cat((cont_P_out.unsqueeze(1), disc_P_out.unsqueeze(1)), dim=1)
-        p_out = torch.mean(p_out, dim=1)
-        
-        cont_C_out = self.linear_cont_C(cont_C)
-        disc_C_out = self.embedding_c(disc_C)
-        c_out = torch.cat((cont_C_out.unsqueeze(1), disc_C_out.unsqueeze(1)), dim=1)
-        c_out = torch.mean(c_out, dim=1)
-        
-        out = torch.add(p_out, c_out)
-        
-        return out
+        self.model_type = 'Transformer'
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        self.encoder = nn.Embedding(ntoken, d_model)
+        self.d_model = d_model
+        self.decoder = nn.Linear(d_model, ntoken)
 
-class PETransformer(nn.Transformer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        self.init_weights()
 
-        # Modified Positional Encoding
-        self.pos_encoder = nn.Embedding(d_model, d_model)
+    def init_weights(self) -> None:
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
 
-    def forward(self, src, tgt):
+    def forward(self, src: torch.Tensor, src_mask: torch.Tensor) -> torch.Tensor:
+        """
+        Arguments:
+            src: Tensor, shape ``[seq_len, batch_size]``
+            src_mask: Tensor, shape ``[seq_len, seq_len]``
+
+        Returns:
+            output Tensor of shape ``[seq_len, batch_size, ntoken]``
+        """
         src = self.encoder(src) * math.sqrt(self.d_model)
         src = self.pos_encoder(src)
         output = self.transformer_encoder(src, src_mask)
         output = self.decoder(output)
         return output
+
+def generate_square_subsequent_mask(sz: int) -> torch.Tensor:
+    """Generates an upper-triangular matrix of ``-inf``, with zeros on ``diag``."""
+    return torch.triu(torch.ones(sz, sz) * float('-inf'), diagonal=1)
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
 
 class SVR(torch.nn.Module):
     def __init__(self):
