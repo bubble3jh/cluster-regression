bafa11a6e04fc1b32c8a1b164dd32177e80fd92a
bubble3jh <bubble3jh@naver.com>
2023-09-05 06:16:14 +0000

    sweep ready

--- sh/cevae.sh
+++ sh/cevae.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-GPU_IDS=(3 4 5 6 7)  # 사용할 GPU ID 리스트
+GPU_IDS=(1 2 3 4 5 6)  # 사용할 GPU ID 리스트
 IDX=0
-target_group=-1
-for regressor_epoch in 2000
+num_epochs=200
+for lambda1 in 0.1 1 10
 do
-  for diffusion_time_steps in 2000
+  for lambda2 in 0.1 1 10
   do
-    for train_num_steps in 16 # 32
+    for lambda3 in 0.1 1 10
     do
-      for train_lr in 8e-5 # 8e-6
-        do
-          for train_fold in 0 1 2 3 4
-          do
+      for learning_rate in 1e-2 1e-3 1e-4
+      do
           # 현재 GPU ID 선택
-          CUDA_VISIBLE_DEVICES=${GPU_IDS[$IDX]} /mlainas/bubble3jh/anaconda3/envs/ppg/bin/python main.py \
-          --regressor_epoch ${regressor_epoch} \
-          --diffusion_time_steps ${diffusion_time_steps} \
-          --sample_only \
-          --train_fold ${train_fold} \
-          --target_group ${target_group} \
-          --t_scheduling "uniform" &  # 백그라운드에서 실행
-
+          CUDA_VISIBLE_DEVICES=${GPU_IDS[$IDX]} cevae.py \
+          --learning_rate ${learning_rate} \
+          --lambda1 ${lambda1} \
+          --lambda2 ${lambda2} \
+          --lambda3 ${lambda3} \
+          --num_epochs ${num_epochs}
+          
           # GPU ID를 다음 것으로 변경
           IDX=$(( ($IDX + 1) % ${#GPU_IDS[@]} ))
 
@@ -29,10 +26,8 @@ do
           if [ $IDX -eq 0 ]; then
             wait
           fi
-        done
-      done  
     done
   done
 done
 
-wait  # 마지막 배치의 모든 작업이 완료될 때까지 기다림
+wait 
